01bb148dc5a4dd60419438d47486c0454bf79318
Shoheicode <ucodejasonirie@gmail.com>
2021-03-16 16:48:03 -0700

    drew oval

--- Game.java
+++ Game.java
@@ -1,7 +1,18 @@
+
 import javax.swing.JFrame;
+import java.awt.Color;
+import java.awt.Graphics;
+
 public class Game extends JFrame{
+
     public Game(){
         setBounds(0,0, 1000,800);
         setVisible(true);
+    }
+
+    public void paint(Graphics graphics){
+        super.paint(graphics);
+        graphics.setColor(Color.red);
+        graphics.fillOval(500, 400, 100, 50);
     }
 }
