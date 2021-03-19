5df4d91280d8547427b5e59f2532b7bef4cb4d4c
Ian Dai <56898254+IanWearsHat@users.noreply.github.com>
2021-03-19 01:18:01 -0700

    Client updated to fit multiple clients

--- network/ClientSide.java
+++ network/ClientSide.java
@@ -4,9 +4,7 @@ import java.io.*;
 import java.net.*;
 
 public class ClientSide implements Runnable {
-    
-    private final String REMOTEADDRESS = "45.51.187.16";
-
+    volatile boolean kill = false;
     public ClientSide() {
         
     }
@@ -20,7 +18,8 @@ public class ClientSide implements Runnable {
             /* Local address is just the address of the client
             remote address is the open ip of the server that the client connects to */
             InetAddress localAddress = InetAddress.getLocalHost();
-            
+            String remoteAddress = "45.51.187.16";
+
             /* Creates a new socket and binds it to the client's address with an open port */
             Socket echoSocket = new Socket();
             echoSocket.bind(new InetSocketAddress(localAddress, 0));
@@ -28,7 +27,7 @@ public class ClientSide implements Runnable {
             /* Attempts to connect to the remote address at the port specified.
             Port is the same as the port specified in port forwarding for router. */
             System.out.println("Attempting Connection...");
-            echoSocket.connect(new InetSocketAddress(REMOTEADDRESS, 9696));
+            echoSocket.connect(new InetSocketAddress("192.168.1.6", 9696));
             System.out.println("Connected to server!");
 
             /* Creates input and output streams for the socket and creates an input stream from the keyboard so the user can provide input */
@@ -39,11 +38,24 @@ public class ClientSide implements Runnable {
             /* Waits for the user to input something in the terminal. 
             When the user hits the return key, the input is sent to the server through the out stream (out.println(userInput)). 
             All the server does is return that input to the client through the input stream. (in.readLine()) */
+            new Thread(() -> {
+                while (!kill) {
+                    try {
+                        System.out.println(in.readLine());
+                    }
+                    catch (Exception e) {
+
+                    }
+                    
+                }
+            }).start();
+
             String userInput;
             while ((userInput = stdIn.readLine()) != null) {
                 out.println(userInput);
-                System.out.println("echo: " + in.readLine());
+                // System.out.println(in.readLine());
             }
+            kill = true;
 
         }
         catch (ConnectException e) {
@@ -55,9 +67,10 @@ public class ClientSide implements Runnable {
             System.exit(1);
         }
     }
-    
+
     @Override
     public void run() {
         startClient();
     }
+    
 }
