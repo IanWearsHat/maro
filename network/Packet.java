177513eccde0f9f8dd82095604734cd82fa015f2
Ian Dai <56898254+IanWearsHat@users.noreply.github.com>
2021-03-20 01:00:38 -0700

    creation of packet class to prepare for updating positions and other states of players on the client side

---
+++ network/Packet.java
@@ -0,0 +1,27 @@
+package network;
+
+// just an information class to be serialized and sent to clients from client handlers
+public class Packet {
+
+    public String message;
+
+    public int clientID; //what client sent this packet
+    public int x; //x position
+    public int y; //y position
+    public boolean attackState; //placeholder boolean just to test if multiple data types are sent correctly
+
+    public int type; // for the client to determine if the packet sent was a message packet or a update player packet.
+
+    public Packet(int type, String message) {
+        this.type = type;
+        this.message = message;
+    }
+
+    public Packet(int type, int clientID, int x, int y, boolean attackState) {
+        this.type = type;
+        this.clientID = clientID;
+        this.x = x;
+        this.y = y;
+        this.attackState = attackState;
+    }
+}
