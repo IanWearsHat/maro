65e3645216dfa1beb37b6923c24faf4170303336
Shoheicode <ucodejasonirie@gmail.com>
2021-03-23 20:35:00 -0700

    Added Video Please test if it can work on other computers.

--- Main.java
+++ Main.java
@@ -1,10 +1,14 @@
 import Game.Game;
+import Game.VideoTest;
+import javafx.application.Application;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("maromaromaromaro");
-        Game game = new Game();
-        Thread gameThread = new Thread(game);
-        gameThread.start();
+        VideoTest a = new VideoTest();
+        VideoTest.begin();
+        // Game game = new Game();
+        // Thread gameThread = new Thread(game);
+        // gameThread.start();
     }
 }
