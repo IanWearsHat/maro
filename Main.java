d7782453a561dacb603b0c8fbaae0dff17f4e2fb
Ian Dai <56898254+IanWearsHat@users.noreply.github.com>
2021-03-28 20:19:08 -0700

    Jframe created with placeholder buttons & toolbar

--- Main.java
+++ Main.java
@@ -1,5 +1,71 @@
+import java.awt.*;
+import java.awt.event.*;
+
+import javax.swing.JButton;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+import javax.swing.JPopupMenu;
+import javax.swing.JToolBar;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("maromaromaromaro");
+        Window w = new Window();
+        JPanel p = new JPanel(new BorderLayout());
+
+        //setting up toolbar
+        JToolBar bar = new JToolBar();
+        bar.setFloatable(false);
+
+        JButton fileB = new JButton("File");  
+        bar.add(fileB);  
+        JButton editB = new JButton("Edit");
+        bar.add(editB);  
+        bar.addSeparator();
+
+
+        JPopupMenu fileMenu = new JPopupMenu();
+
+        JMenuItem importOption = new JMenuItem("Import map file");
+        importOption.addActionListener( new ActionListener() {
+            public void actionPerformed(ActionEvent ae) {
+                System.out.println("importing");
+            }
+        });
+        fileMenu.add(importOption);
+
+
+        JMenuItem exportOption = new JMenuItem("Export map file");
+        exportOption.addActionListener( new ActionListener() {
+            public void actionPerformed(ActionEvent ae) {
+                System.out.println("exporting");
+            }
+        });
+        fileMenu.add(exportOption);
+
+        //makes it so the file button drops down the menu for file
+        fileB.addActionListener( new ActionListener() {
+            public void actionPerformed(ActionEvent ae) {
+                fileMenu.show(fileB, 0, fileB.getHeight());
+            }
+        });
+        bar.add(fileMenu);
+
+        // setting up placeholder buttons, b1 represents options for tile selection, b2 represents the actual screen where you edit tiles
+        JButton b1 = new JButton("b1");
+        //                  new Dimension(length, height)
+        b1.setPreferredSize(new Dimension(200, 100));
+        JButton b2 = new JButton("b2");
+
+        // adding the toolbar and placeholder buttons to the panel
+        p.add(bar, BorderLayout.PAGE_START);
+
+        p.add(b1, BorderLayout.LINE_START);
+        p.add(b2, BorderLayout.CENTER);
+
+        //adding the panel to the window
+        w.add(p);
+        w.pack();
+        w.setVisible(true);
     }
 }
---
+++ Window.java
@@ -0,0 +1,37 @@
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.awt.Graphics2D;
+import java.awt.event.*;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Window extends JFrame {
+    public static final int width = 1600;
+    public static final int height = 900;
+    public static final int scale = 2;
+
+    private int fps = 60;
+    private long targetTime = 1000/fps;
+
+    private BufferedImage image;
+    private Graphics2D g;
+
+    public Window() {
+        super();
+        setPreferredSize(new Dimension(width, height));
+        setFocusable(true);
+        setResizable(false);
+        requestFocus();
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    private void init() {
+    }
+
+    public void runFrame() {
+        init();
+    }
+
+}
