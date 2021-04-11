81f81dfee113b61a30f836939d7433ad4fcdf2dc
IanWearsHat <ianbbdai@gmail.com>
2021-04-11 12:47:49 -0700

    client stops when server stops

--- network/ClientSide.java
+++ network/ClientSide.java
@@ -11,7 +11,6 @@ import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.net.SocketException;
 import java.net.SocketTimeoutException;
-import java.nio.channels.ClosedByInterruptException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import network.packet.*;
@@ -20,7 +19,6 @@ public class ClientSide implements Runnable {
     private static final Logger LOGGER = Logger.getLogger( ClientSide.class.getName() );
 
     private volatile boolean kill = false;
-    private boolean running = true;
 
     public ClientSide() {}
 
@@ -54,8 +52,19 @@ public class ClientSide implements Runnable {
             /* Waits for the user to input something in the terminal. 
             When the user hits the return key, the input is sent to the server through the out stream (out.println(userInput)). */
             Thread userInputThread = new Thread(() -> {
-                try {
-                    while (running) {
+                boolean isRunning = true;
+                while (!Thread.interrupted() && isRunning) {
+                    try {
+                        if (!stdIn.ready()) { //https://8thlight.com/blog/francesca-sadikin/2019/08/01/handling-blocking-threads-in-java.html
+                            try {
+                                Thread.sleep(1);
+                                continue;
+                            }
+                            catch (InterruptedException e) {
+                                return;
+                            }
+                        }
+
                         System.out.print("You: ");
                         String userInput = stdIn.readLine();
                         if (!userInput.equals(null)) {
@@ -63,47 +72,41 @@ public class ClientSide implements Runnable {
                             // out.reset();
                         }
                         else {
-                            running = false;
+                            isRunning = false;
                         }
                     }
-                }
-                catch (ClosedByInterruptException e) {
-                    LOGGER.info("interrupted");
-                }
-                catch (Exception e) {
+                    catch (Exception e) {
 
+                    }
                 }
             });
+            userInputThread.start();
 
             //this thread has to run so that the client can wait for a message from the server
             //while also waiting for an input from the user.
-            Thread messageReceiver = new Thread(() -> {
-                while (!kill) {
-                    try {
-                        Packet receivedPacket = (Packet) in.readObject();
-                        if (receivedPacket instanceof MessagePacket) {
-                            System.out.println(((MessagePacket) receivedPacket).message);
-                        }
-                        else if (receivedPacket instanceof PlayerPacket) {
-                            System.out.println(((PlayerPacket) receivedPacket).x);
-                            // this is how the client would update other players' positions on the client's screen
-                            // it would receive other clients' positions from the server and render them accordingly on its screen.
-                        }
+            while (!kill) {
+                try {
+                    Packet receivedPacket = (Packet) in.readObject();
+                    if (receivedPacket instanceof MessagePacket) {
+                        System.out.println(((MessagePacket) receivedPacket).message);
                     }
-                    catch (ClassNotFoundException e) {
-                        LOGGER.log(Level.SEVERE, "Packet wasn't a Packet object, killing input reader thread.", e);
-                        kill = true;
-                        userInputThread.interrupt();
-                        running = false;
-                    } 
-                    catch (IOException e) {
-                        LOGGER.log(Level.SEVERE, "Could not read packet, killing input reader thread.", e);
-                        kill = true;
-                        userInputThread.interrupt();
-                        running = false;
+                    else if (receivedPacket instanceof PlayerPacket) {
+                        System.out.println(((PlayerPacket) receivedPacket).x);
+                        // this is how the client would update other players' positions on the client's screen
+                        // it would receive other clients' positions from the server and render them accordingly on its screen.
                     }
                 }
-            });
+                catch (ClassNotFoundException e) {
+                    LOGGER.log(Level.SEVERE, "Packet wasn't a Packet object, killing input reader thread.", e);
+                    kill = true;
+                    userInputThread.interrupt();
+                } 
+                catch (IOException e) {
+                    LOGGER.log(Level.SEVERE, "Could not read packet, killing input reader thread.", e);
+                    kill = true;
+                    userInputThread.interrupt();
+                }
+            }
         }
         catch (ConnectException | SocketTimeoutException e) {
             LOGGER.log(Level.SEVERE, "No server found. Exiting...", e);
@@ -114,8 +117,7 @@ public class ClientSide implements Runnable {
         catch (IOException e) {
             e.printStackTrace();
         }
-        
-        kill = true;
+
     }
 
     @Override
